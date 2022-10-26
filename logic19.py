0ec9cc3b99a0fda1a025b1aafe01669c51b1f1f9
Akhmad <akhmadhurshetov@gmail.com>
2022-10-27 00:05:57 +0500

    clear

--- logic19.py
+++ logic19.py
@@ -1,15 +1,15 @@
-# def main(x):
-#     """
-#     Given an integer x, return true if x is palindrome integer.
-#     An integer is a palindrome when it reads the same backward as forward.
+def main(x):
+    """
+    Given an integer x, return true if x is palindrome integer.
+    An integer is a palindrome when it reads the same backward as forward.
 
-#     Args:
-#         x(int): parameter x
-#     Returns:
-#         bool: answer
-#     """
+    Args:
+        x(int): parameter x
+    Returns:
+        bool: answer
+    """
 
-#     return
+    return
 
 # n=int(input("Enter number:"))
 # temp=n
